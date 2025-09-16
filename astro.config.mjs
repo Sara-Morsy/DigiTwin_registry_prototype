43483a4ee3d966b0273141fa12418f7de6f1d212
Sara-Morsy <saram.morsy@gmail.com>
2025-09-16 03:56:52 +0100

    add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://example.com', // change for GitHub Pages
+  // GitHub Pages URL for your repo
+  site: 'https://Sara-Morsy.github.io/DigiTwin_registry_prototype/',
+  // Base path = repo name
+  base: '/DigiTwin_registry_prototype/',
   integrations: [tailwind(), react()],
 });
